d0b80aef10494a68f6f6438598c11b96406ee784
anusha-arunn <anusha.arunn@gmail.com>
2024-03-26 00:02:42 -0700

    Modified contac.html

--- contact.php
+++ contact.php
@@ -1,15 +1,21 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve form data
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+    $name = isset($_POST['full_name']) ? $_POST['full_name'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $subject = isset($_POST['subject']) ? $_POST['subject'] : '';
+    $message = isset($_POST['message']) ? $_POST['message'] : '';
 
     // File attachment
-    $file = $_FILES['attachment'];
-    $attachment = chunk_split(base64_encode(file_get_contents($file['tmp_name'])));
-    $filename = $file['name'];
+    $file = isset($_FILES['attachment']) ? $_FILES['attachment'] : null;
+
+    if ($file && $file['error'] === UPLOAD_ERR_OK) {
+        $attachment = chunk_split(base64_encode(file_get_contents($file['tmp_name'])));
+        $filename = $file['name'];
+    } else {
+        $attachment = '';
+        $filename = '';
+    }
 
     // Email headers
     $headers = "From: $name <$email>\r\n";
@@ -21,15 +27,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $body .= "Content-Type: text/plain; charset=\"UTF-8\"\r\n";
     $body .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
     $body .= "$message\r\n";
-    $body .= "--boundary\r\n";
-    $body .= "Content-Type: application/octet-stream; name=\"$filename\"\r\n";
-    $body .= "Content-Transfer-Encoding: base64\r\n";
-    $body .= "Content-Disposition: attachment\r\n\r\n";
-    $body .= "$attachment\r\n";
+    
+    if ($attachment) {
+        $body .= "--boundary\r\n";
+        $body .= "Content-Type: application/octet-stream; name=\"$filename\"\r\n";
+        $body .= "Content-Transfer-Encoding: base64\r\n";
+        $body .= "Content-Disposition: attachment\r\n\r\n";
+        $body .= "$attachment\r\n";
+    }
+
     $body .= "--boundary--";
 
     // Send email
-    $success = mail('anchu.nandi@gmail.com.com', $subject, $body, $headers);
+    $success = mail('anchu.nandi@gmail.com', $subject, $body, $headers);
 
     if ($success) {
         echo "Email sent successfully!";
